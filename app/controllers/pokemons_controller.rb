b098f8257c617838ccc80e46f96bcfa6da6b67a1
kleekich <kleekich@berkeley.edu>
2015-10-24 23:07:57 -0700

    Pokemon migration file fixed

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -1,2 +1,18 @@
 class PokemonsController < ApplicationController
+=begin 
+Grab the id of the Pokemon from the parameters and set that Pokemon's trainer to be the current trainer logged in.
+What's great about Devise (the user authentication gem we already have set up) is that it predefines a method to
+ give us the current user logged in - or in this case, current trainer. To grab the current trainer, use current_trainer. 
+ You can use this in controllers and views.
+Don't forget to save the changes to the Pokemon!!!
+Redirect to the home page
+=end 
+	def capture
+		@pokemon = Pokemon.find(params[:id])
+		@pokemon.trainer_id = current_trainer.id
+		Pokemon.save
+		redirect_to root_path
+
+	end
+
 end
--- db/migrate/20151025060532_create_pokemons.rb
+++ db/migrate/20151025060532_create_pokemons.rb
@@ -1,8 +1,8 @@
 class CreatePokemons < ActiveRecord::Migration
   def change
     create_table :pokemons do |t|
-      t.string, :name
-      t.integer, :level
+      t.string :name
+      t.integer :level
       t.integer :trainer_id
 
       t.timestamps null: false
