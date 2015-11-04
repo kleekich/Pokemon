e5531cea1e54e7c3587542d3221831f398cddfd0
kleekich <kleekich@berkeley.edu>
2015-11-03 21:08:22 -0800

    Error Fixed

--- app/controllers/pokemons_controller.rb
+++ app/controllers/pokemons_controller.rb
@@ -7,54 +7,48 @@ What's great about Devise (the user authentication gem we already have set up) i
 Don't forget to save the changes to the Pokemon!!!
 Redirect to the home page
 =end 
+	
 	def capture
-		@pokemon = Pokemon.find(params[:id])
-		@pokemon.trainer_id = current_trainer.id
-		@pokemon.save
+		pokemon = Pokemon.find(params[:id])
+		pokemon.trainer_id = current_trainer.id
+		pokemon.save
 		redirect_to root_path
 
 	end
 
 	def damage
-		@pokemon = Pokemon.find(params[:id])
-
-		@pokemon.health -= 10
-		if @pokemon.health <=0
-			Pokemon.delete(@pokemon)
+		pokemon = Pokemon.find(params[:id])
+		pokemon.health -= 10
+		if pokemon.health <=0
+			pokemon.destroy
+         redirect_to trainer_path(id: current_trainer.id)
 		end
-		@pokemon.save
+		pokemon.save
 		redirect_to trainer_path(id: current_trainer.id)
-		
+    end
 
-   end
-
-   def new
+    def new
 		@pokemon = Pokemon.new
-
-   	    render "new"
+      	render "new"
    		
-   end
+    end
 
-   def create
-   	if Pokemon.new(name: params[:pokemon][:name], trainer_id: current_trainer.id).valid?
-   		pokemon = Pokemon.new(name: params[:pokemon][:name], trainer_id: current_trainer.id)
-   		pokemon.save
-   		redirect_to trainer_path(id: current_trainer.id)
-   	else
-   		@pokemon = Pokemon.new(name: params[:pokemon][:name], trainer_id: current_trainer.id)
-   		flash[:error] = @pokemon.errors.full_messages.to_sentence << "Error: Duplicate Name or No Name"
-   		redirect_to new_path
+    def create
+   		if Pokemon.new(name: params[:pokemon][:name], trainer_id: current_trainer.id).valid?
+       		pokemon = Pokemon.new(name: params[:pokemon][:name], trainer_id: current_trainer.id)
+   	  		pokemon.save
+   	   		redirect_to trainer_path(id: current_trainer.id)
+   	 	else
+   			@pokemon = Pokemon.new(name: params[:pokemon][:name], trainer_id: current_trainer.id)
+   			flash[:error] = @pokemon.errors.full_messages.to_sentence << "Error: Duplicate Name or No Name"
+ 	  		redirect_to new_path
 
+   	 	end
+    end
+
+
+    def user_params
+  		params.require(:pokemon).permit(:name)
    	end
-
-   	
-
-
-   end
-
-   def user_params
-  	params.require(:pokemon).permit(:name)
-   end
 	
-
 end
